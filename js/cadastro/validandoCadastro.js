23d803122ea25f4812c0917697bf67f3f03f9b1f
GabrielsouzaDev <101714272+Osouzaa@users.noreply.github.com>
2023-01-09 20:47:31 -0300

    Update validandoCadastro.js

--- js/cadastro/validandoCadastro.js
+++ js/cadastro/validandoCadastro.js
@@ -2,13 +2,10 @@ const validandoCadastro = () => {
   const login = document.getElementById("name").value;
   const email = document.getElementById("email").value;
   const senha = document.getElementById("senha").value;
-  const confirmarSenha = document.getElementById('confirm-senha').value;
-
-
-
+  const confirmarSenha = document.getElementById("confirm-senha").value;
 
   if (login !== "" && email !== "" && senha !== "" && confirmarSenha !== "") {
-     document.getElementById("btt-cadastrar").disabled = false;
+    document.getElementById("btt-cadastrar").disabled = false;
     return;
   }
   document.getElementById("btt-cadastrar").disabled = true;
