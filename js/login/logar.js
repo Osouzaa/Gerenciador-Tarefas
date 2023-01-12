adbf6557e1d6026d07a74179c054ad567e043b5e
GabrielsouzaDev <101714272+Osouzaa@users.noreply.github.com>
2023-01-11 22:21:20 -0300

    Update logar.js

--- js/login/logar.js
+++ js/login/logar.js
@@ -2,26 +2,54 @@ const logar = async () => {
   const login = document.getElementById("usuario").value;
   const password = document.getElementById("senha").value;
 
+  const border_Senha = document.getElementById("senha");
+  const border_User = document.getElementById("usuario");
+
   const apiResponse = await fetch("http://localhost:3000/login");
   const users = await apiResponse.json();
 
   let persons = users.filter((perso) => {
-    return perso.login == login;
+    return perso.login === login;
   });
 
   let senha = users.filter((perso) => {
-    return perso.senha == password;
+    return perso.senha === password;
   });
 
   if (persons.length == 1 && senha.length == 1) {
     location.href = "pages/tarefas.html";
-  } else if (persons.length == 1 && senha.length == 0) {
-    document.getElementById("msgError").innerHTML = "Digite sua senha! ";
-  } else if (persons.length == 0 && senha.length == 0) {
-    document.getElementById("msgError").innerHTML = "Digite seu usuario e senha";
+  } else {
+    document.getElementById("msgError").innerHTML =
+      "Usuario ou senha incorreta!";
+    border_User.classList.add("has-error");
+    border_Senha.classList.add("has-error");
   }
+};
 
+const validandoUser = () => {
+  const border_Senha = document.getElementById("senha");
+  const border_User = document.getElementById("usuario");
 
+  const login = document.getElementById("usuario").value;
+  const password = document.getElementById("senha").value;
+
+  if (login !== "" && password !== "") {
+    border_User.classList.remove("has-error");
+    border_Senha.classList.remove("has-error");
+    document.getElementById("msgError").innerHTML = "";
+  } else if (login !== "" && password == "") {
+    border_User.classList.remove("has-error");
+    border_Senha.classList.add("has-error");
+    document.getElementById("msgError").innerHTML = "Digite sua senha! ";
+  } else if (login == "" && password !== "") {
+    border_User.classList.add("has-error");
+    border_Senha.classList.remove("has-error");
+  } else if (login == "" && password == "") {
+    border_User.classList.add("has-error");
+    border_Senha.classList.add("has-error");
+    document.getElementById("msgError").innerHTML =
+      "Digite seu usuario e senha";
+  }
 };
 
 const limparCampo = () => {
@@ -31,5 +59,3 @@ const limparCampo = () => {
     document.getElementById("msgError").innerHTML = "";
   }, 2000);
 };
-
-
