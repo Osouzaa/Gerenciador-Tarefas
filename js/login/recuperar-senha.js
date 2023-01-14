7f2381da5a02a8545ca80ef24389c71f1cd5d851
GabrielsouzaDev <101714272+Osouzaa@users.noreply.github.com>
2023-01-14 14:43:12 -0300

    Add files via upload

---
+++ js/login/recuperar-senha.js
@@ -0,0 +1,19 @@
+const validaUser = async () => {
+  const email = document.getElementById("email").value.trim();
+  const msgUser = document.getElementById("senha-rec");
+
+  const apiResponse = await fetch("http://localhost:3000/login");
+  const users = await apiResponse.json();
+
+  let endereço = users.filter((user) => {
+    return user.email === email;
+  });
+
+  if (endereço.length > 0) {
+    msgUser.innerHTML = "Usuario: " + endereço[0].login + " <br>" + "" + "Senha: " + endereço[0].senha;
+  } else if(email == "") {
+    msgUser.innerHTML = "Digite seu e-mail!";
+  } else if(endereço.length == 0) {
+    msgUser.innerHTML = "Não encontramos o email digitado!";
+  }
+};
