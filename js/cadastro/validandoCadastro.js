de7d32ba3048801732a5037658432c86c8219391
GabrielsouzaDev <101714272+Osouzaa@users.noreply.github.com>
2023-01-14 11:45:39 -0300

    Update validandoCadastro.js

--- js/cadastro/validandoCadastro.js
+++ js/cadastro/validandoCadastro.js
@@ -17,7 +17,7 @@ const validaUser = async () => {
   const email = document.getElementById("email").value.trim();
   const msgUser = document.getElementById("user");
 
-  const apiResponse = await fetch("http://localhost:3000/login");
+  const apiResponse = await fetch("https://meu-json-server.vercel.app/login");
   const users = await apiResponse.json();
 
   let user = users.filter((user) => {
